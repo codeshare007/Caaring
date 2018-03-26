a84a032e2b117c5b37e897f808dfdbb91ec0da7c
robertlopez9216 <robert.lopez160192@gmail.com>
2018-03-26 21:30:17 +0530

    initial commit

--- software/settings.py
+++ software/settings.py
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'caaring/static')
 
 STATIC_URL = '/static/'
 
